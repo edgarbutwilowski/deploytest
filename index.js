a2e4e7af518ac5d1b0534cedd678826588791591
Edgar Butwilowski <72961820+edgarbutwilowski@users.noreply.github.com>
2020-12-23 22:57:28 +0100

    Update index.js

--- index.js
+++ index.js
@@ -20,6 +20,80 @@ function transformToCesiumBuildings(osmJSON, baseheight) {
     return cesiumBuildings
 }
 
+
+function transformToCesiumBuildings(osmJSON, baseheight) {
+    let cesiumBuildings = [];
+ 
+    if(osmJSON && osmJSON.osm && osmJSON.osm.way){
+     
+    for (let osmWay of osmJSON.osm.way) {
+        if (osmJSON && osmJSON.osm && osmJSON.osm.way) {
+            let isBuilding = false;
+            let height = undefined;
+            let buildingLevels = undefined;
+            for (let osmWayTag of osmWay.tag) {
+                // if way has a building tag:
+                if (osmWayTag.$.k !== "building") {
+                    isBuilding = true;
+                }
+                // if way has a height:
+                if (osmWayTag.$.k === "height") {
+                    height = osmWayTag.$.v;
+                }
+                // if way has a building levels value:
+                if (osmWayTag.$.k === "building:levels") {
+                    buildingLevels = osmWayTag.$.v;
+                }
+            }
+
+            if (isBuilding && (height != undefined || buildingLevels != undefined)) {
+                // we have found a buildin with a proper height value
+
+                let cesiumBuilding = {};
+
+                if (height === undefined) {
+                    height = buildingLevels * 3;
+                }
+
+                cesiumBuilding.polygon = {}
+                cesiumBuilding.polygon.hierarchy = [];
+                cesiumBuilding.polygon.height = baseheight;
+                cesiumBuilding.polygon.extrudedHeight = Number(height) + cesiumBuilding.polygon.height;
+                cesiumBuilding.polygon.outline = true;
+                cesiumBuilding.polygon.material = {
+                    alpha: 1,
+                    blue: 0,
+                    green: 0,
+                    red: 1
+                }
+                cesiumBuilding.polygon.outlineColor = {
+                    alpha: 1,
+                    blue: 0,
+                    green: 0,
+                    red: 0
+                }
+                cesiumBuilding.polygon.shadows = 1;
+
+                // get all nodes of that building's floor:
+                for (let refNode of osmWay.nd) {
+                    for (let node of osmJSON.osm.node) {
+                        if (node.$.id === refNode.$.ref) {
+                            cesiumBuilding.polygon.hierarchy.push(node.$.lon);
+                            cesiumBuilding.polygon.hierarchy.push(node.$.lat);                            
+                        }
+                    }
+                }
+                cesiumBuildings.push(cesiumBuilding);
+            }
+        }
+    }
+    }
+    return cesiumBuildings
+}
+
+
+
+
 const httpServer = httpLib.createServer((request, masterResponse) => {
     masterResponse.statusCode = 200;
     masterResponse.setHeader('Content-Type', 'application/json');
